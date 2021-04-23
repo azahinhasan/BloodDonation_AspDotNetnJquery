03c39c236e17bef0c2e53b57c13325e92c33b7c2
A H Zahin <azahinhasan@gmail.com>
2021-04-23 18:08:36 +0600

    Fast Push

---
+++ Backend/BloodDonation/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BloodDonation.Models;
+using BloodDonation.Auth;
+using System.Web;
+using System.Web.Http.Cors;
+
+namespace BloodDonation.Controllers
+{
+  /*  [RoutePrefix("api/home")]*/
+    public class HomeController : ApiController
+    {
+        BloodDonationDBEntities context = new BloodDonationDBEntities();
+
+
+        [Route("api/admin")]
+        public IHttpActionResult Get()
+        {
+            var user = context.userInfoes.Where(x => x.Type == "Admin").ToList();
+
+            // return StatusCode(HttpStatusCode.NoContent);
+            //return Ok(context.userInfoes.ToList());
+            return Ok(user);
+        }
+
+
+        [Route("api/admin/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            userInfo user = context.userInfoes.Find(id);
+            if (user == null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+
+            return Ok(user);
+        }
+
+        [Route("api/login"),HttpPost] 
+        public IHttpActionResult login(userInfo user)
+        {
+            var type = "none";
+
+            //bool data = context.userInfoes.Any(x => x.Email == user.Email && x.Password == user.Password);
+            var data = context.userInfoes.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault<userInfo>();
+
+            if (data != null)
+            {
+                type = data.Type;
+
+                return Json(type);
+
+
+            }
+
+            return Json(type);
+        }
+
+    }
+}
